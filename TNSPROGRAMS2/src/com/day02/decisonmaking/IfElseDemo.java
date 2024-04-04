955703ff24fbebf2cc2b19b6aabe6efcb17342d8
Vaibhav <Vaibhav@DESKTOP-ANOURM5>
2024-04-04 20:27:31 +0530

    Day02Programs

---
+++ TNSPROGRAMS2/src/com/day02/decisonmaking/IfElseDemo.java
@@ -0,0 +1,20 @@
+package com.day02.decisonmaking;
+
+public class IfElseDemo {
+	
+public static void main(String[] args) {
+
+		
+		int age=22;
+		
+		if(age >=18)
+		{
+			System.out.println("You are eligible for voting ");
+		}
+		else {
+			System.out.println("Sorry ; you need to wait ");
+		}
+		System.out.println("Yoy Cant vote");
+	}
+
+}
