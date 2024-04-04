620c0532417e7b9fcc4258e238ea4841291a1889
Vaibhav <Vaibhav@DESKTOP-ANOURM5>
2024-04-04 20:53:23 +0530

    Practice Program

---
+++ PracticePrograms/src/com/loginform/nestedif/LoginForm.java
@@ -0,0 +1,29 @@
+package com.loginform.nestedif;
+
+public class LoginForm {
+	
+	public static void main(String[] args) {
+		
+		String Username = "Vaibhav";
+		String Password = "123456";
+		
+		if (Username == "Vaibhav") {
+			
+			
+			
+			if (Password == "123456") {
+				
+				System.out.println("Login Sucssefully");
+				
+				
+			}else {
+				
+				System.out.println("Password Is Incorrect");
+				
+			}
+			
+		}
+		
+	}
+
+}
